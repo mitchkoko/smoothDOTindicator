b1abfc8ef0fbe2b87f0e91af780de81f85917130
Mitch Koko <mitchkoko@Mitchs-MacBook-Pro.local>
2021-11-12 01:20:55 +1100

    first commit

---
+++ android/app/src/main/kotlin/com/example/smoothdotindicator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.smoothdotindicator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/homepage.dart
@@ -0,0 +1,52 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:smoothdotindicator/pages/page_1.dart';
+import 'package:smoothdotindicator/pages/page_2.dart';
+import 'package:smoothdotindicator/pages/page_3.dart';
+import 'package:smoothdotindicator/pages/page_4.dart';
+
+class HomePage extends StatelessWidget {
+  final _controller = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[200],
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          // page view
+          SizedBox(
+            height: 500,
+            child: PageView(
+              controller: _controller,
+              children: const [
+                Page1(),
+                Page2(),
+                Page3(),
+                Page4(),
+              ],
+            ),
+          ),
+
+          // dot indicators
+          SmoothPageIndicator(
+            controller: _controller,
+            count: 4,
+            effect: JumpingDotEffect(
+              activeDotColor: Colors.deepPurple,
+              dotColor: Colors.deepPurple.shade100,
+              dotHeight: 30,
+              dotWidth: 30,
+              spacing: 16,
+              //verticalOffset: 50,
+              jumpScale: 3,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
